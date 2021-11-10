7349dca28e3d1b7a968f51aa2f47d0e3cc9d73ae
Jyothis P <33348514+Jyothis-P@users.noreply.github.com>
2021-11-10 17:58:24 +0530

    added gaurds for user routes.

--- bookApp/src/app/Shared/guards/userGuard/auth.guard.ts
+++ bookApp/src/app/Shared/guards/userGuard/auth.guard.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ToastService } from '../../toasts/services/toast.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  
-  constructor(private router:Router){}
+
+  constructor(private router: Router, private toastService: ToastService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    
-    if(sessionStorage.getItem('UserToken'))
+
+    if (sessionStorage.getItem('UserToken'))
       return true;
-    
-    else  
+
+    else {
+      this.toastService.show("You must be logged in to view this page.", { classname: "bg-danger text-light", delay: 3000 });
       this.router.navigate(['auth/login']);
       return false;
+    }
   }
-  
+
 }
