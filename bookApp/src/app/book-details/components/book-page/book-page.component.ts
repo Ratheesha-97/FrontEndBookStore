74d00c9707f08edff7a92e2ef1f46a443eb9cc2b
Jyothis P <33348514+Jyothis-P@users.noreply.github.com>
2021-11-11 11:14:42 +0530

    Add functionalities to Book-Details page.

--- bookApp/src/app/book-details/components/book-page/book-page.component.ts
+++ bookApp/src/app/book-details/components/book-page/book-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from 'src/app/book/services/book.service';
+import { ToastService } from 'src/app/shared/toasts/services/toast.service';
+import { UserService } from 'src/app/user/services/user.service';
 import { ReviewService } from '../../services/review.service';
 
 @Component({
@@ -9,23 +11,60 @@ import { ReviewService } from '../../services/review.service';
   styleUrls: ['./book-page.component.css']
 })
 export class BookPageComponent implements OnInit {
-book:any;
-reviews:any;
-reviewList: any;
-rating=0;
-clicked=false;
-  constructor(private route:ActivatedRoute,private bookService:BookService,private reviewService:ReviewService) { }
+  book: any;
+  reviews: any;
+  reviewList: any;
+  rating = 0;
+  clicked = false;
+  constructor(private route: ActivatedRoute, private bookService: BookService, private reviewService: ReviewService, private userService: UserService, private toastService: ToastService) { }
 
   async ngOnInit(): Promise<void> {
     let BId = this.route.snapshot.paramMap.get('id');
-    this.book=await this.bookService.getBookById(BId);
+    this.book = await this.bookService.getBookById(BId);
     console.log(this.book)
-    this.reviews=await this.reviewService.getReviews();
+    this.reviews = await this.reviewService.getReviews();
     console.log(this.reviews)
-    this.reviewList=this.reviews.filter((c: { BookId: string | null;Review1: string | null; })=>(c.BookId==BId && c.Review1!=null))
+    this.reviewList = this.reviews.filter((c: { BookId: string | null; Review1: string | null; }) => (c.BookId == BId && c.Review1 != null))
     console.log(this.reviewList[0].UserInfo.FName);
     console.log(this.reviewList);
-    this.rating=this.book['BRating'];
+    this.rating = this.book['BRating'];
+  }
+
+
+  addToCartBtn(id: number) {
+    this.userService.addBookToCart(id)
+      .subscribe(
+        (res: any) => {
+          console.log("Book added to cart.");
+          this.toastService.show("Book added to cart.", { classname: 'bg-success text-light', delay: 3000 })
+        },
+        (err: any) => {
+          let errorMessage = "Error adding book to cart.";
+          if (err.status == 500)
+            errorMessage = "Book already in cart.";
+
+          console.error(err);
+          this.toastService.show(errorMessage, { classname: 'bg-danger text-light', delay: 3000 })
+        }
+      )
+  }
+
+  addToWishlistBtn(id: number) {
+    this.userService.addBookToWishlist(id)
+      .subscribe(
+        (res: any) => {
+          console.log("Book added to wishlist.");
+          this.toastService.show("Book added to wishlist.", { classname: 'bg-success text-light', delay: 3000 })
+        },
+        (err: any) => {
+          let errorMessage = "Error adding book to wishlist.";
+          if (err.status == 500)
+            errorMessage = "Book already in wishlist.";
+
+          console.error(err);
+          this.toastService.show(errorMessage, { classname: 'bg-danger text-light', delay: 3000 })
+        }
+      )
   }
 
 }
